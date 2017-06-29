f051f2aa80fdbb1c125a16327f427b24414bbf58
pecorina <constanza.pecori@gmail.com>
2017-06-29 10:58:35 -0400

    no pude u.u

--- js/query.js
+++ js/query.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-	$("button").click(function(e){
-		e.preventDefault();
-		var name = $("#nombre").val();
-		
-	});
+$(document).ready(function() {
+    $("input").click(function(e){ //agrega tarea al click
+        e.preventDefault();
+        var tarea = $("#tarea").val();
+
+    $('#check').append('<div class="cajatarea"><input type="checkbox" id="otra"/>' + tarea + '<button class="boton">Remove</button></div>');
+
+    $('button').click(function(){
+        $(this).parent().remove();//funcion para remover al click
+    });
+
+    });
+    
 });
